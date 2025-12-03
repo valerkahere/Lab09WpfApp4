3faa5fba5b55dbd1c261738d43677b6ca613eb14
Valerii Navalnyi <adultvalerka@gmail.com>
2025-12-03 23:54:45 +0000

    Window_Loaded Completed with filling list box

--- Exam19-20/MainWindow.xaml.cs
+++ Exam19-20/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Exam19_20
 {
@@ -16,26 +7,62 @@ namespace Exam19_20
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Player> allPlayers = new List<Player>();
+        List<Player> selectedPlayers = new List<Player>();
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
-        List<Player> players = new List<Player>();
+        private List<Player> CreatePlayers()
+        {
+            List<Player> players = new List<Player>();
 
-        string[] firstNames = {
+            string[] firstNames = {
                 "Adam", "Amelia", "Ava", "Chloe", "Conor", "Daniel", "Emily",
                 "Emma", "Grace", "Hannah", "Harry", "Jack", "James",
                 "Lucy", "Luke", "Mia", "Michael", "Noah", "Sean", "Sophie"};
-
-        string[] lastNames = {
+            // 20
+            string[] lastNames = {
                 "Brennan", "Byrne", "Daly", "Doyle", "Dunne", "Fitzgerald", "Kavanagh",
                 "Kelly", "Lynch", "McCarthy", "McDonagh", "Murphy", "Nolan", "O'Brien",
                 "O'Connor", "O'Neill", "O'Reilly", "O'Sullivan", "Ryan", "Walsh"
-        };
+            };
 
-        Random random = new Random();
-        
+            // Create players
+            Random random = new Random();
+
+            for (int i = 0; i < firstNames.Length; i++)
+            {
+                // generate random date where age is 20-30
+                // now is 2025
+                int currentYear = 2025;
+                int desiredAge = random.Next(20, 31);
+
+                Player p = new Player()
+                {
+                    FirstName = firstNames[random.Next(0, firstNames.Length)],
+                    Surname = lastNames[random.Next(0, lastNames.Length)],
+                    DateOfBirth = new DateTime(currentYear - desiredAge, 1, 1)
+                };
+
+                players.Add(p);
+
+
+            }
+
+            return players;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create Players
+            allPlayers = CreatePlayers();
+
+            // Displaying Players in List Box
+            lbxAll.ItemsSource = allPlayers;
+        }
     }
 
 }

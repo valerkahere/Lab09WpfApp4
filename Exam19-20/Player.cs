ffdac78f675faeeaa17fc948f60d0b02a67729a1
Valerii Navalnyi <adultvalerka@gmail.com>
2025-12-03 23:29:17 +0000

    Q3 and 4. Created class and enum

--- Exam19-20/Player.cs
+++ Exam19-20/Player.cs
@@ -1,22 +1,5 @@
 ﻿namespace Exam19_20
 {
-    internal class Player
-    {
-        public string FirstName { get; set; } = string.Empty;
-        public string Surname { get; set; } = string.Empty;
-
-        public Position Position { get; set; }
-
-        public DateTime DateOfBirth { get; set; }
-
-        public int Age { get; set; }
-        
-        public Player()
-        {
-
-        }
-
-    }
 
     public enum Position
     {
@@ -25,4 +8,46 @@
         Midfielder,
         Forward
     }
+    internal class Player
+    {
+        // Properties - use shorthand unless there is a need for longer properties
+        public string FirstName { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+
+        public Position PreferredPosition { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        private int _age;
+
+        public int Age
+        {
+            get
+            {
+                // Determine age and account for birthday that has not happened yet
+                _age = DateTime.Now.Year - DateOfBirth.Year;
+                if (DateOfBirth.DayOfYear >= DateTime.Now.DayOfYear) // then birth hasn't happened yet
+                {
+                    _age--;
+                }
+                return _age;
+            }
+        }
+
+
+        // Constructors
+        public Player()
+        {
+
+        }
+
+        // Methods
+        public override string ToString()
+        {
+            // Need to convert Preferred Position to string because it's enum
+            return $"{FirstName} {Surname} ({Age}) {PreferredPosition.ToString().ToUpper()}";
+        }
+    }
+
+
 }
